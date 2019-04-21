ffea21280c268046d5833eb4af1b271d413d8510
hillary <hillary_ssemakula@student.uml.edu>
2019-04-21 00:13:54 -0400

    ...

--- js/shapeEditor.js
+++ js/shapeEditor.js
@@ -5,7 +5,8 @@ var startPolyDraw = false;
 var isMouseDown = false;
 var canvas1 = new CanvasObject(canvas); //This is the global canvas, we'll be working with.
 var selectedShape = -100; //index to note selected shape.
-
+var xChangeCanvas = 0; //variables to keep track of change in mouse position within canavs: used to ploy transformations.
+var yChangeCanvas = 0;
 
 function getMouseCoordinates(event) {
   //get mouse position
@@ -17,12 +18,23 @@ function getMouseCoordinates(event) {
 
 function Shape(type, coordinates) {
   this.type = type;
-  this.coordinates = coordinates
+  this.coordinates = coordinates;
+  this.selected = false;
+  this.thickness = 3;
+  this.color = "#2E86C1";
 }
 
 //Draw function for shape object.
 Shape.prototype.draw = function(context) {
   this.path = new Path2D(); //path object very important to check whether point lies in path.
+  context.lineWidth = this.thickness;
+  context.strokeStyle = this.color;
+  if (this.selected) {
+    context.shadowColor = 'green';
+    context.shadowBlur = 7;
+  } else {
+    context.shadowBlur = 0;
+  }
   context.beginPath(this.path);
   switch (String(this.type)) {
     case "line":
@@ -75,8 +87,6 @@ Shape.prototype.draw = function(context) {
 function CanvasObject(canvas) {
   this.canvas = canvas;
   this.context = canvas.getContext('2d');
-  this.context.strokeStyle = "#1B4F72";
-
 }
 
 
@@ -85,11 +95,22 @@ CanvasObject.prototype.draw =
   function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     for (var i = 0; i < shapes.length; i++) {
-      console.log(shapes);
       shapes[i].draw(this.context);
     }
   }
 
+/* To be used for poly translation */
+
+canvas.addEventListener('mousemove', setCoordChanges);
+
+function setCoordChanges(event) {
+  xChangeCanvas = event.movementX;
+  yChangeCanvas = event.movementY;
+}
+/* ---------------------------------------------------------- */
+
+
+//JQUERY
 $(function() {
 
 
@@ -109,6 +130,8 @@ $(function() {
     }
     var coordinates = getMouseCoordinates(event);
     makeSelection(coordinates);
+    //this is function is also used to draw polys, so draw shouldn't be called here if we're making poly
+    if (polyType == "") canvas1.draw();
   });
 
 
@@ -117,6 +140,7 @@ $(function() {
   $("#canvas").mousedown(function(event) {
     isMouseDown = true;
     var coordinates = getMouseCoordinates(event);
+    makeSelection(coordinates); //if mouse down near another object, it can be selected.
     initPoly(coordinates); //initilize a polygon if poly button pressed
   });
 
@@ -124,6 +148,7 @@ $(function() {
     var coordinates = getMouseCoordinates(event); //get current coordinates.
     var x = coordinates[0];
     var y = coordinates[1];
+
 
 
     //if user is dragging to define poly change last two coordintes and draw
@@ -136,11 +161,15 @@ $(function() {
     //if shape is selected,
     if (selectedShape != -100 && isMouseDown) {
       //if new point is within shape then move: prevents selected shape from jumping from one point to another.
-      if (canvas1.context.isPointInPath(shapes[selectedShape].path, x, y)) {
+      if (canvas1.context.isPointInPath(shapes[selectedShape].path, x, y) || canvas1.context.isPointInStroke(shapes[selectedShape].path, x, y)) {
         if (shapes[selectedShape].type == "polyline" || shapes[selectedShape].type == "polygon") {
-          translatePoly(selectedShape, x, y);
+          //movementX and Y used because polygons and lines don't have a center(x,y) that can easily be incremented or decremented
+          //so change in x and y cursor is used to increment/decrement all sides.
+          translatePoly(selectedShape, xChangeCanvas, yChangeCanvas);
         } else shapes[selectedShape] = makeShape(shapes[selectedShape].type, x, y);
+        shapes[selectedShape].selected = true //draw border and shadow as shape is moved to show selection.
       }
+
       canvas1.draw();
     }
 
@@ -200,7 +229,7 @@ $(function() {
 function makeShape(shapeType, x, y) {
   switch (shapeType) {
     case "line":
-      return new Shape("line", [x - 50, y, x + 50, y]);
+      return new Shape("line", [x - 50, y, x, y]);
       break;
     case "triangle":
       return new Shape("triangle", [x + 50, y + 50, x, y - 50, x - 50, y + 50]);
@@ -242,30 +271,42 @@ function translatePoly(shapeIndex, x, y) {
 //changes last coordinate of poly to given coordinates
 function changeLastVertex(x, y) {
   var shapeIndex = shapes.length - 1; //last shape added
-  var xIndexToChange = shapes[shapeIndex].coordinates.length - 2; //last coordintes added by mouse down i.e 0,0, these are to be cahged to new x and y
+  var xIndexToChange = shapes[shapeIndex].coordinates.length - 2; //last coordintes added by mouse down as place holders. i.e -1000,-1000, these are to be cahged to new x and y
   var yIndexToChange = shapes[shapeIndex].coordinates.length - 1;
+  var xBefore = xIndexToChange - 2; //x,y before the last point.(before place holder values -1000)
+  var yBefore = yIndexToChange - 2;
 
-  shapes[shapeIndex].coordinates[xIndexToChange] = x; //coordinates changed
-  shapes[shapeIndex].coordinates[yIndexToChange] = y;
+  //check if new point to be added is the same as last point added.
+  if ((xBefore >= 0 && shapes[shapeIndex].coordinates[xBefore] == x) && shapes[shapeIndex].coordinates[yBefore] == y) {
+    shapes[shapeIndex].coordinates.pop(); //remove place holder points -1000, -1000, also there is nothing to add, last point same as new point.
+    shapes[shapeIndex].coordinates.pop();
+  } else {
+    shapes[shapeIndex].coordinates[xIndexToChange] = x; //coordinates changed
+    shapes[shapeIndex].coordinates[yIndexToChange] = y;
+  }
 }
 
 
-//make selection and updated flag to reflect selected shape.
+//make selection and update flag to reflect selected shape.
 function makeSelection(coordinates) {
   //if there are shapes that have been drawn and we're not in poly mode.
   if (shapes.length > 0 && polyType == "") {
-    //check if pointer lies in any path of any shape stored so far.
+    //for all shape paths check if pointer lies in any path of any shape stored so far.
     for (var i = 0; i < shapes.length; i++) {
-      if (canvas1.context.isPointInPath(shapes[i].path, coordinates[0], coordinates[1])) {
-        console.log("yes");
+      if (canvas1.context.isPointInPath(shapes[i].path, coordinates[0], coordinates[1]) ||
+        canvas1.context.isPointInStroke(shapes[i].path, coordinates[0], coordinates[1])) {
+
+        if (selectedShape != -100) {
+          shapes[selectedShape].selected = false;
+        }
         selectedShape = i;
-        console.log(selectedShape);
-        break;
-      } else {
-        console.log("no");
-        console.log(selectedShape);
-        selectedShape = -100
+        shapes[selectedShape].selected = true;
+        return;
       }
+    }
+    if (selectedShape != -100) {
+      shapes[selectedShape].selected = false;
+      selectedShape = -100
     }
   }
 }
@@ -295,13 +336,14 @@ function initPoly(coordinates) {
 }
 
 
-
-
-
 //called by double click this method finalizes the polygon or polyline drawing.
 function finalizePoly(coordinates) {
+  if (polyType == "polyline" && shapes[shapes.length - 1].coordinates.length < 4) {
+    alert("A ployline cannot be a dot");
+    return;
+  }
   //check if polygon drawn is more than 3 sides.(2 * 3 vertices)
-  if (polyType == "polygon" && shapes[shapes.length - 1].coordinates.length <= 6) {
+  else if (polyType == "polygon" && shapes[shapes.length - 1].coordinates.length < 6) {
     alert("A ploygon cannot have less than 3 sides");
     return;
   }
@@ -312,11 +354,12 @@ function finalizePoly(coordinates) {
     var lastYIndex = shapes[shapeIndex].coordinates.length - 1;
 
     //if last point clicked is close to starting point, join to starting point else add
-    if (Math.abs(shapes[shapeIndex].coordinates[0]) - Math.abs(shapes[shapeIndex].coordinates[lastXIndex]) <= 5 ||
-      Math.abs(shapes[shapeIndex].coordinates[1]) - Math.abs(shapes[shapeIndex].coordinates[lastYIndex]) <= 5) {
+    if (Math.abs(shapes[shapeIndex].coordinates[0] - shapes[shapeIndex].coordinates[lastXIndex]) <= 5 &&
+      Math.abs(shapes[shapeIndex].coordinates[1] - shapes[shapeIndex].coordinates[lastYIndex]) <= 5) {
       shapes[shapeIndex].coordinates[lastXIndex] = shapes[shapeIndex].coordinates[0];
       shapes[shapeIndex].coordinates[lastYIndex] = shapes[shapeIndex].coordinates[1];
     } else {
+
       //else add another vertex and join to start point.
       shapes[shapeIndex].coordinates.push(shapes[shapeIndex].coordinates[0]);
       shapes[shapeIndex].coordinates.push(shapes[shapeIndex].coordinates[1]);
@@ -326,9 +369,6 @@ function finalizePoly(coordinates) {
   startPolyDraw = false;
   polyType = "";
 }
-
-
-
 
 /* This function iterates through all html elements with the class shapeButton and innitializes them as draggable */
 function attachDraggableAbility() {
